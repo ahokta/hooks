cf2401996661d5d7d305a1f5d0773b17f87e80fc
Anton Herber <anton.herber@okta.com>
2020-12-08 18:07:56 +0100

    Script to get AD groups and members for OU groups

--- getUserIDandDN.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-url=https://id3.antonherber.de/api/v1/users/
-apikey=00CRx596VFz3Q39iK5ZnEYu5_xXHG9q9FJ3EvXZJrC
-# just some url
-
-curl --location --request GET ${url}  --header 'Accept: application/json' --header 'Content-Type: application/json' --header 'Authorization: SSWS ' ${apikey}
